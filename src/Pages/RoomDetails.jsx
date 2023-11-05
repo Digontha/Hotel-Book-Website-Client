65a57a94e75fc97271558e69386603ac95665396
Digontha <digonthodj@gmail.com>
2023-11-05 21:53:51 +0600

    create book button for booking hotel for auth user

--- src/Pages/RoomDetails.jsx
+++ src/Pages/RoomDetails.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+
+import { useContext } from 'react';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../AuthProvider/AuthProvider';
+import swal from 'sweetalert';
+
 
 const RoomDetails = () => {
     const rooms = useLoaderData()
-
+   const {user} = useContext(AuthContext)
+   const navigate = useNavigate()
     const { room_name,
         room_description,
         price_per_night,
@@ -16,6 +21,16 @@ const RoomDetails = () => {
         room_image4 } = rooms || {};
 
     console.log(room_name);
+
+    const handleBookNowClick = () => {
+        if (user) {
+            swal("Logged", "You book successfully", "success");
+        } else {
+            // User is not authenticated, redirect to the login page
+            navigate('/login');
+        }
+    };
+
     return (
         <>
             <div className=' flex justify-center gap-10 mt-8'>
@@ -23,21 +38,26 @@ const RoomDetails = () => {
                     <img className='rounded-xl' src={room_image1} alt="" />
 
                     <div className='mt-10'>
-                <div className="card  bg-neutral text-neutral-content space-y-8">
-                    <div className="card-body">
-                        <h2 className="card-title">{room_name}</h2>
-                        <p className='w-[50%]'>{room_description}</p>
-                        <p className='font-bold text-lg'><span  className='font-bold text-xl underline'>Room Size:</span> ${room_size}</p>
-                        <p><span  className='font-bold text-xl underline'>special offers:</span> <span className='w-[50%]'>{special_offers}</span></p>
-                        <p className='font-bold text-lg'><span  className='font-bold text-xl underline'>Price:</span> ${price_per_night}</p>
-                        <div className="card-actions ">
-                            {availability==="available"?<button className="btn w-full btn-success btn-outline">Book now</button>:
-                            <button className="w-full  disabled">room not available</button>
-                            }
+                        <div className="card  bg-neutral text-neutral-content space-y-8">
+                            <div className="card-body">
+                                <h2 className="card-title">{room_name}</h2>
+                                <p className='w-[50%]'>{room_description}</p>
+                                <p className='font-bold text-lg'><span className='font-bold text-xl underline'>Room Size:</span> {room_size}</p>
+                                <p><span className='font-bold text-xl underline'>special offers:</span> <span className='w-[50%]'>{special_offers}</span></p>
+                                <p className='font-bold text-lg'><span className='font-bold text-xl underline'>Price:</span> ${price_per_night}</p>
+                                 <input className='text-black font-medium p-4 rounded-2xl' type="date" name="" id="" />
+                                    <div className="card-actions ">
+
+                                        {availability === "available" ? <button onClick={handleBookNowClick} className="btn w-full btn-success btn-outline">Book now</button> :
+                                            <button className="w-full  disabled">room not available</button>
+                                        }
+
+                                    </div>
+                              
+
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
                 </div>
 
                 <div>
@@ -47,7 +67,7 @@ const RoomDetails = () => {
                 </div>
             </div>
 
-            
+
 
         </>
     );
